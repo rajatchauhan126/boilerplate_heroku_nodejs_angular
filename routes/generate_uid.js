6f40de203faa808707613eb4459fd12eac2341ca
rajatchauhan126 <rajatchauhan126@gmail.com>
2020-03-29 10:50:51 +0530

    remove comment

--- routes/generate_uid.js
+++ routes/generate_uid.js
@@ -6,16 +6,12 @@ var userCount = 0;
 /* GET a guid. */
 router.post('/counter', function (req, res, next) {
     // var strUid = uid.sync(18);
-    console.log(req.body.count);
 
     if (req.body.count > userCount) {
         userCount = req.body.count + 1;
     } else {
         userCount = userCount + 1;
     }
-
-    console.log("I received a request to refresh the counter ", userCount);
-
     res.json({ count: userCount });
 });
 
